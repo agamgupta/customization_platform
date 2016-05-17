01da1a6a3b4ba2fe0beb79619a136c3d80e9f0fc
Agam Gupta <agamgupta2018@u.northwestern.edu>
2016-05-17 16:40:03 -0500

    Added server.js and package.json

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var mongoose = require('mongoose')
+var methodOverride = require('method-override')
+var morgan = require("morgan")
+var bodyParser = require("body-parser")
+var port = process.env.PORT || 8080
+
+//app.use(express.static(__dirname)); // To be able to return static objects - such as images, html, js files kept locally
+
+app.get('*', function(req, res){
+	res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port)
+console.log('This is a basic show of hands that the server is working')
